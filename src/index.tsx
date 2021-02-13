aaa90abfde5e432587f5c93adc555cf49571eace
Sajjad Taghilou <sajjad.taghilou@outlook.com>
2021-02-14 00:01:26 +0330

    clear & add ts

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'Styles/reset.css';
+import 'Styles/base.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
