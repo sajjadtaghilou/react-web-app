f58e687f3bc6b735ef753ff4202738b4f8fd8de4
Sajjad Taghilou <sajjad.taghilou@outlook.com>
2021-04-02 14:01:15 +0430

    change query devtools position

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ function App() {
     <>
       <ReactQueryDevtools
         panelProps={{ style: { direction: "ltr", textAlign: "left" } }}
+        position="top-left"
       />
       <Routes />
     </>
